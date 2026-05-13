f5bc7ed763f345dff3fd7a7961fdb142d8d76b52
Fancy2209 <64917206+Fancy2209@users.noreply.github.com>
2026-05-13 14:23:51 +0000

    Ammend previous commit as there is no need to include SDL twice

--- utils/finderrors.c
+++ utils/finderrors.c
@@ -30,7 +30,6 @@ static void *lookup(const char *fnname, void *unused)
 #define WIN32_LEAN_AND_MEAN 1
 #include <windows.h>
 #include <malloc.h>  // for alloca().
-#include <SDL2/SDL.h>
 #define snprintf SDL_snprintf
 #else
 #include <dirent.h>
